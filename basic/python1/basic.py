96a87139be51cb594a8cafa378a530dba446aa5c
ot-nemoto <nemoto@opentone.co.jp>
2024-07-15 12:35:41 +0000

    基礎編: プログラムからの呼び出し１

---
+++ basic/python1/basic.py
@@ -0,0 +1,28 @@
+import boto3
+import json
+
+#モデルの設定
+#modelId="anthropic.claude-3-sonnet-20240229-v1:0"
+modelId="anthropic.claude-3-haiku-20240307-v1:0"
+
+#プロンプトの設定
+user_prompt = "カレーの作り方を説明してください"
+
+bedrock = boto3.client('bedrock-runtime')
+body = json.dumps(
+    {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 4000,
+        "messages": [{"role": "user","content": user_prompt}]
+    }
+)
+
+# Bedrockの呼び出し
+response = bedrock.invoke_model(body=body,modelId=modelId)
+
+# Bedrock呼出し結果の抽出
+response_body = json.loads(response.get('body').read())
+answer = response_body["content"][0]["text"]
+
+# 結果の出力
+print(answer)
