d7c3222f5053fe1db321568a0b6f50fd08cc1e0d
ot-nemoto <nemoto@opentone.co.jp>
2024-07-16 12:57:03 +0000

    チャットアプリケーション編: Amazon Bedrock のストリーミング応答

---
+++ chat/stream/bedrock-stream.py
@@ -0,0 +1,21 @@
+import boto3
+import json
+
+bedrock = boto3.client(service_name='bedrock-runtime')
+
+body = json.dumps({
+    "anthropic_version": "bedrock-2023-05-31",
+    "max_tokens": 4000,
+    "messages": [{"role": "user", "content": "カレーの作り方を説明してください"}]
+})
+
+response = bedrock.invoke_model_with_response_stream(modelId="anthropic.claude-3-haiku-20240307-v1:0", body=body)
+
+stream = response.get("body")
+if stream:
+    for event in stream:
+        chunk = event.get("chunk")
+        if chunk:
+            chunk_json = json.loads(chunk.get("bytes").decode())
+            if chunk_json["type"]=="content_block_delta":
+                print(chunk_json["delta"]["text"], end="", flush=True)
---
+++ chat/stream/langchain-stream.py
@@ -0,0 +1,10 @@
+from langchain_aws import ChatBedrock
+import streamlit as st
+
+# chainの定義
+LLM = ChatBedrock(model_id="anthropic.claude-3-haiku-20240307-v1:0", model_kwargs={"max_tokens": 4000})
+chain = LLM
+
+## ストリーミングで実行
+for chunk in chain.stream("カレーの作り方を説明してください"):
+    print(chunk.content, end="", flush=True)
