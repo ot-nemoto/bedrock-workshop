09b4f07fb8f9e26c9bd9f310ffbf794ee4da1001
ot-nemoto <nemoto@opentone.co.jp>
2024-07-16 13:05:57 +0000

    参考: Converse API の使用

---
+++ converse/README.md
@@ -0,0 +1,5 @@
+# [参考: Converse API の使用](https://catalog.us-east-1.prod.workshops.aws/workshops/7271111a-22bd-40e7-971a-817b0c083c67/ja-JP/converse)
+
+```sh
+python3 basic.py
+```
---
+++ converse/basic.py
@@ -0,0 +1,21 @@
+import boto3
+
+#モデルの設定
+#modelId="anthropic.claude-3-sonnet-20240229-v1:0"
+modelId="anthropic.claude-3-haiku-20240307-v1:0"
+
+#プロンプトの設定
+user_prompt = "カレーの作り方を説明してください"
+
+bedrock = boto3.client("bedrock-runtime")
+messages = [{"role": "user", "content": [{"text": user_prompt}]}]
+inferenceConfig = {"maxTokens": 4000}
+
+# Bedrockの呼び出し
+response = bedrock.converse(modelId=modelId, messages=messages, inferenceConfig=inferenceConfig)
+
+# Bedrock呼出し結果の抽出
+answer = response["output"]["message"]["content"][0]["text"]
+
+# 結果の出力
+print(answer)
