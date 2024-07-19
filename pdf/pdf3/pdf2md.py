ae6fd6cb5bba3029bca0d42f88349fdb1ec19bea
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-07-19 11:28:07 +0900

    Markdown 変換と解析

--- pdf/pdf3/pdf2md.py
+++ pdf/pdf3/pdf2md.py
@@ -1,9 +1,11 @@
+import os
+
 import pymupdf4llm
 
 # PDFファイルのパス
-pdf_path='sample.pdf'
+pdf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sample.pdf")
 
 # PDFファイルからMarkdownに変換
-md_text = pymupdf4llm.to_markdown(pdf_path,pages=range(0,3),margins=(0,0,0,0))
+md_text = pymupdf4llm.to_markdown(pdf_path, pages=range(0, 3), margins=(0, 0, 0, 0))
 
 print(md_text)
